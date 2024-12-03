9e0c878adfd1222db832dc4a5b35440b804e95c0
Rayhan Pervej <103215390+Rayhan-Pervej@users.noreply.github.com>
2024-12-04 00:23:25 +0600

    updated all

---
+++ lib/screens/auth/login.dart
@@ -0,0 +1,190 @@
+import 'package:flutter/material.dart';
+import 'package:kisukinen/theme/color.dart';
+import 'package:kisukinen/theme/text.dart';
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: MyColor.containerPink,
+      body: Stack(
+        children: [
+          Positioned(
+            top: 100,
+            left: 30,
+            child: SizedBox(
+              height: 300,
+              width: 300,
+              child: Image.asset("assets/mascot.png"),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              decoration: const BoxDecoration(
+                  color: MyColor.containerWhite,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(15),
+                      topRight: Radius.circular(15))),
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: _logintButton(),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _logintButton() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Sign up or log in",
+          style: TextDesign().navText.copyWith(fontSize: 20),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Text(
+          "Select your preferred method to continue",
+          style: TextDesign()
+              .bodyText
+              .copyWith(fontWeight: FontWeight.w400, fontSize: 12),
+        ),
+        const SizedBox(
+          height: 7,
+        ),
+        _google(),
+        _facebook(),
+        _email(),
+        const SizedBox(
+          height: 10,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Don't have an account?",
+              style: TextDesign().bodyText.copyWith(
+                    fontSize: 10,
+                    fontWeight: FontWeight.w400,
+                  ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 4),
+                child: Text(
+                  'Sign Up!',
+                  style: TextDesign().buttonText.copyWith(
+                      fontSize: 12,
+                      color: MyColor.containerPink,
+                      fontWeight: FontWeight.w600),
+                ),
+              ),
+            )
+          ],
+        )
+      ],
+    );
+  }
+
+  Widget _google() {
+    return OutlinedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+          backgroundColor: MyColor.containerWhite,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          padding: const EdgeInsets.symmetric(vertical: 10)),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Image.asset(
+              'assets/google.png',
+              height: 20,
+              width: 20,
+            ),
+            Text(
+              "Continue with Google",
+              style:
+                  TextDesign().bodyText.copyWith(fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _facebook() {
+    return OutlinedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+          backgroundColor: MyColor.containerBlue,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          padding: const EdgeInsets.symmetric(vertical: 10),
+          side: const BorderSide(color: MyColor.containerBlue)),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Image.asset(
+              'assets/facebook.png',
+              height: 20,
+              width: 20,
+            ),
+            Text(
+              "Continue with Facebook",
+              style: TextDesign().bodyText.copyWith(
+                  fontWeight: FontWeight.bold, color: MyColor.containerWhite),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _email() {
+    return OutlinedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+          backgroundColor: MyColor.containerWhite,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          padding: const EdgeInsets.symmetric(vertical: 10)),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Image.asset(
+              'assets/email.png',
+              height: 20,
+              width: 20,
+            ),
+            Text(
+              "Continue with email",
+              style:
+                  TextDesign().bodyText.copyWith(fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/theme/color.dart
@@ -0,0 +1,40 @@
+
+import 'package:flutter/material.dart';
+
+class MyColor{
+  static const appbarColor = Color(0xffffffff);
+  static const scaffoldColor = Color(0xfff1f2f6);
+
+  // Button Color
+  static const buttonPink = Color.fromARGB(255, 189, 14, 81);
+  static const buttonBlue = Color.fromARGB(255, 19, 72, 248);
+  static const buttonBlack = Color.fromARGB(255, 6, 10, 43);
+
+  // Container
+  static const containerWhite = Colors.white;
+  static const containerBlue = Color(0xff5271ff);
+  static const containerRed = Color(0xffff5252);
+  static const containerGray = Color.fromARGB(143, 53, 55, 56);
+  static const containerYellow = Color.fromARGB(255, 255, 234, 188);
+  static const containerGreen = Color(0xff00bf63);
+  static const containerPink = Color.fromARGB(255, 255, 43, 114);
+
+  // Alert
+  static const alertBlue = Color(0xff256ed1);
+  static const alertRed = Color(0xffff5252);
+
+  //Text field
+  static const fieldGray = Color(0xfff1f2f6);
+
+  // Header Text
+  static const headerBlue = Color(0xff3232d2);
+  static const headerBlack = Colors.black;
+
+  // Normal Text/line
+  static const black = Colors.black;
+  static const graySoft = Color(0xff5c6166);
+  static const gray = Color(0xff38383a);
+  static const green = Color(0xff00bf63);
+  static const white = Colors.white;
+  static const blue = Color(0xff256ed1);
+}
